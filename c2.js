78772652081e40837257957301e4e067e95babcb
tuiuiu5 <113807351+tuiuiu5@users.noreply.github.com>
2022-10-31 16:15:04 -0300

    Add files via upload

---
+++ c2.js
@@ -0,0 +1,190 @@
+var cores = [ "AliceBlue",
+"AntiqueWhite",
+"Aqua",
+"Aquamarine",
+"Azure",
+"Beige",
+"Bisque",
+"Black",
+"BlanchedAlmond",
+"Blue",
+"BlueViolet",
+"Brown",
+"BurlyWood",
+"CadetBlue",
+"Chartreuse",
+"Chocolate",
+"Coral",
+"CornflowerBlue",
+"Cornsilk",
+"Crimson",
+"Cyan",
+"DarkBlue",
+"DarkCyan",
+"DarkGoldenRod",
+"DarkGray",
+"DarkGrey",
+"DarkGreen",
+"DarkKhaki",
+"DarkMagenta",
+"DarkOliveGreen",
+"DarkOrange",
+"DarkOrchid",
+"DarkRed",
+"DarkSalmon",
+"DarkSeaGreen",
+"DarkSlateBlue",
+"DarkSlateGray",
+"DarkSlateGrey",
+"DarkTurquoise",
+"DarkViolet",
+"DeepPink",
+"DeepSkyBlue",
+"DimGray",
+"DimGrey",
+"DodgerBlue",
+"FireBrick",
+"FloralWhite",
+"ForestGreen",
+"Fuchsia",
+"Gainsboro",
+"GhostWhite",
+"Gold",
+"GoldenRod",
+"Gray",
+"Grey",
+"Green",
+"GreenYellow",
+"HoneyDew",
+"HotPink",
+"IndianRed",
+"Indigo",
+"Ivory",
+"Khaki",
+"Lavender",
+"LavenderBlush",
+"LawnGreen",
+"LemonChiffon",
+"LightBlue",
+"LightCoral",
+"LightCyan",
+"LightGoldenRodYellow",
+"LightGray",
+"LightGrey",
+"LightGreen",
+"LightPink",
+"LightSalmon",
+"LightSeaGreen",
+"LightSkyBlue",
+"LightSlateGray",
+"LightSlateGrey",
+"LightSteelBlue",
+"LightYellow",
+"Lime",
+"LimeGreen",
+"Linen",
+"Magenta",
+"Maroon",
+"MediumAquaMarine",
+"MediumBlue",
+"MediumOrchid",
+"MediumPurple",
+"MediumSeaGreen",
+"MediumSlateBlue",
+"MediumSpringGreen",
+"MediumTurquoise",
+"MediumVioletRed",
+"MidnightBlue",
+"MintCream",
+"MistyRose",
+"Moccasin",
+"NavajoWhite",
+"Navy",
+"OldLace",
+"Olive",
+"OliveDrab",
+"Orange",
+"OrangeRed",
+"Orchid",
+"PaleGoldenRod",
+"PaleGreen",
+"PaleTurquoise",
+"PaleVioletRed",
+"PapayaWhip",
+"PeachPuff",
+"Peru",
+"Pink",
+"Plum",
+"PowderBlue",
+"Purple",
+"RebeccaPurple",
+"Red",
+"RosyBrown",
+"RoyalBlue",
+"SaddleBrown",
+"Salmon",
+"SandyBrown",
+"SeaGreen",
+"SeaShell",
+"Sienna",
+"Silver",
+"SkyBlue",
+"SlateBlue",
+"SlateGray",
+"SlateGrey",
+"Snow",
+"SpringGreen",
+"SteelBlue",
+"Tan",
+"Teal",
+"Thistle",
+"Tomato",
+"Turquoise",
+"Violet",
+"Wheat",
+"White",
+"WhiteSmoke",
+"Yellow",
+"YellowGreen"
+];
+var alpha_colors = [cores[Math.floor(Math.random() * cores.length)]];
+var i = 0;
+while(i<9){
+    if(alpha_colors[i].includes(alpha_colors[i-1])){
+        alpha_colors.pop();
+    }else{ 
+            alpha_colors.push(cores[Math.floor(Math.random() * cores.length)]);
+            i++;}
+}
+alpha_colors.sort();
+var acabou = false;
+var tentativas = 0;
+var coralvo = alpha_colors[Math.floor(Math.random() * alpha_colors.length)];
+
+function do_game() {
+  var adivinha_cor = prompt("Estou pensando em uma dessas cores \n\n" + alpha_colors + "\n\n Em qual delas estou pensando?");
+  tentativas += 1;
+  checaracerto(adivinha_cor);
+}
+
+function checaracerto(adivinha_cor) {
+  if (adivinha_cor == "stop") {
+    return false;
+  }
+  if (cores.indexOf(adivinha_cor) == -1) {
+    alert("Desculpe.  Não reconheci sua cor. \n\n Tente novamente");
+    do_game();
+  }
+  if (adivinha_cor > coralvo) {
+    alert("Você errou, tente novamente. \n\n Dica: Sua cor é alfabeticamente menor que a minha");
+    do_game();
+  }
+  if (adivinha_cor < coralvo) {
+    alert("Você errou, tente novamente. \n\n Hint: Sua cor é alfabeticamente maior que a minha");
+    do_game();
+  }
+  if (cores.indexOf(adivinha_cor) >= 0) {
+    alert("Parabéns! Você acertou a cor! \n\n Você levou " + tentativas + "tentativas pra terminar o jogo! \n\n Você pode ver a cor no plano de fundo");
+  }
+  document.body.style.backgroundColor = coralvo;
+}
